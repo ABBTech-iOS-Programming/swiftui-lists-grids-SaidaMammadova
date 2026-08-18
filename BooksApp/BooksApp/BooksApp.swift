60e72a228ec19d3e89f68b3028204900ec52f8fa
Saida Mammadova <saidaa.mammadova@gmail.com>
2026-08-18 23:14:50 +0400

    add project files and app entry point

---
+++ BooksApp/BooksApp/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppApp.swift
+//  BooksApp
+//
+//  Created by Saida Mammadova on 13.08.26.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
